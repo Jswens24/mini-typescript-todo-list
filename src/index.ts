0d3174fcade2a153ea7d2b3b9c7c4693610d4067
Jessica Swenson <jessicamswens@gmail.com>
2023-05-07 14:26:43 -0600

    added checkbox

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,63 @@
+interface Todo {
+    text: string;
+    completed: boolean;
+};
+
 const btn = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
+const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')! as HTMLUListElement;
+
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo)
 
 
+function readTodos(): Todo[] {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON === null) return [];
+    return JSON.parse(todosJSON);
+}
 
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
 
-btn.addEventListener('click', function () {
-    alert(input.value);
+function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+
+    saveTodos();
     input.value = '';
-});
+}
+
+function createTodo(todo: Todo) {
+    const newLI = document.createElement("LI");
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    })
+
+    newLI.append(todo.text);
+    newLI.append(checkbox)
+    list.append(newLI);
+}
+
+form.addEventListener('submit', handleSubmit)
+
+
+
+// btn.addEventListener('click', function () {
+//     alert(input.value);
+//     input.value = '';
+// });
 
 
 
