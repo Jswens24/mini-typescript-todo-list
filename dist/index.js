1d5fe2d741d518223ea7a8f2b12b671adce5061d
Jessica Swenson <jessicamswens@gmail.com>
2023-05-07 13:21:47 -0600

    started project

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const btn = document.getElementById('btn');
+const input = document.getElementById('todoinput');
+btn.addEventListener('click', function () {
+    alert(input.value);
+    input.value = '';
+});
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn')! as HTMLButtonElement;
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+
+
+
+
+btn.addEventListener('click', function () {
+    alert(input.value);
+    input.value = '';
+});
+
+
+
+
+
